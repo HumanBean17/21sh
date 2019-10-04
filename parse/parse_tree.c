81c86aa08d808926a730406f6272e6a6fdcfa17b
mmarti <beachtelecom42@gmail.com>
2019-10-04 22:40:55 +0300

    murat loh

--- parse/parse_tree.c
+++ parse/parse_tree.c
@@ -11,91 +11,42 @@
 /* ************************************************************************** */
 
 #include "shell.h"
+#include <errno.h>
 
-int		count_redirects(t_tree *tree)
-{
-	int red;
+//void	ft_fopen(char **fname, t_tree *node, int fd[])
+//{
+//	struct stat stbuf;
+//
+//	if (stat(*fname, &stbuf) == 0)
+//	{
+//		if (node->type == TGREAT) {
+//			if ((fd[1] = open(*fname, O_RDWR | O_TRUNC)) < 0)
+//				printf("%s", strerror(errno));
+//		}
+//		else
+//			fd[1] = open(*fname, O_RDWR | O_APPEND);
+//	}
+//	else {
+//		if ((fd[1] = open(*fname, O_CREAT | S_IRWXU | O_RDWR)) < 0)
+//			printf("%s", strerror(errno));
+//	}
+//	fd[0] = -1;
+//	node->left->type = TFILE;
+//}
 
-	red = 0;
-	while (tree && tree->type != TEXEX)
-	{
-		red++;
-		tree = tree->right;
-	}
-	return (red);
-}
-
-void	add_to_fdlist(int fd0, int fd1, int *tmp)
-{
-	while (*tmp >= 0)
-		tmp++;
-	tmp[0] = fd1;
-	tmp[1] = fd0;
-}
-
-int	get_from_fdlist(int *fdlist)
-{
-	static int i;
-
-	i++;
-	return (fdlist[i - 1]);
-}
-
-int *ft_fdmalloc(size_t size)
-{
-	int *res;
-
-	if (!(res = (int *)malloc(sizeof(int) * size)))
-		exit(123);
-	while (size--)
-		res[size] = -1;
-	return (res);
-}
-
-int		*make_fdlist(int count)
-{
-	int fd[2];
-	int *tmp;
-
-	tmp = ft_fdmalloc(count * 2 + 1);
-	while (count--)
-	{
-		pipe(fd);
-		add_to_fdlist(fd[0], fd[1], tmp);
-	}
-	return (tmp);
-}
-
-void redirect(t_tree *tree, int *fdlist)
-{
-	if (!tree)
-		return ;
-	redirect(tree->left, fdlist);
-	if (tree->type == TEXEX)
-	{
-		if (tree->parent->parent == 0 && tree->parent->right != tree)
-			ft_do(tree->val, -1, get_from_fdlist(fdlist));
-		else
-			ft_do(tree->val, get_from_fdlist(fdlist), get_from_fdlist(fdlist));
-	}
-	redirect(tree->right, fdlist);
-}
 
 void	parse_tree(t_tree *tree)
 {
-	int *fdlist;
-
 	if (!tree)
 		return ;
 	if (tree->type == TSEMICOL)
 	{
-		parse_tree(tree->left);
 		parse_tree(tree->right);
+		parse_tree(tree->left);
 	}
-	else if (tree->type != TEXEX)
+	else
 	{
-		fdlist = make_fdlist(count_redirects(tree));
-		redirect(tree, fdlist);
-		waitpid(g_pid, 0, 0);
+		ft_manage(tree, -1);
+		waitpid(-1, 0, 0);
 	}
 }
